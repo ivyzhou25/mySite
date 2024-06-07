d4c0692b379bba98fe5c3a62ac3267b81751fd59
cindyhuang12 <164077493+cindyhuang12@users.noreply.github.com>
2024-06-06 22:32:59 -0700

    Delete mySite directory

--- mySite/pages/form.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django import forms
-from .models import Image
-
-class ImageForm(forms.Form):        
-    prompt = forms.CharField(label="Additional image information:", max_length=100)
-    file = forms.FileField(label="Upload image:")
-    
--- mySite/pages/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-import requests
-
-class Image(models.Model):
-    image = models.ImageField(upload_to='images/')
