f8f2e9618d9ac0a566d75ecc4e485b14348594f9
cindyhuang12 <164077493+cindyhuang12@users.noreply.github.com>
2024-06-09 20:37:29 -0700

    add openai api call

---
+++ pages/openai.py
@@ -0,0 +1,46 @@
+import base64
+import requests
+
+api_key = <api_key>
+
+# Function to encode the image
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
+def process_image(image_path):
+    # Path to your image
+    #image_path = "/Users/ivyzhou/downloads/Artboard_Copy_231.jpg"
+    
+    # Getting the base64 string
+    base64_image = encode_image(image_path)
+    
+    headers = {
+      "Content-Type": "application/json",
+      "Authorization": f"Bearer {api_key}"
+    }
+    
+    payload = {
+      "model": "gpt-4o",
+      "messages": [
+        {
+          "role": "user",
+          "content": [
+            {
+              "type": "text",
+              "text": "What’s in this image?"
+            },
+            {
+              "type": "image_url",
+              "image_url": {
+                "url": f"data:image/jpeg;base64,{base64_image}"
+              }
+            }
+          ]
+        }
+      ],
+      "max_tokens": 300
+    }
+    
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+    return response
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render, redirect
-from django.conf import settings
+from django.shortcuts import render
+from pages.models import Image
 from django.views.decorators.csrf import ensure_csrf_cookie
 from .form import ImageForm
+from django.conf.urls.static import static
+from django.conf import settings
+from pages.openai import encode_image, process_image
 import os
+
 
 def home(request):
     return render(request, "pages/index.html")
@@ -16,22 +20,31 @@ def contact(request):
 
 def product(request):
     if request.method == 'POST':
-        #form = ImageForm(request.POST, request.FILES)
         img = request.FILES["file"]
-        prompt = request.POST.get('prompt')
-        handle_uploaded_file(img)
+        ## call openai to get image description
         
-        return render(request, "pages/product_detail.html", {"MEDIA_URL": settings.MEDIA_URL, "img": img})
+        resp = handle_uploaded_file(img)
+        
+        return render(request, "pages/product_detail.html", {"MEDIA_URL": settings.MEDIA_URL, "img": img, "resp" : resp})
     else:
         form = ImageForm()
-    return render(request, 'pages/product.html', {'form': form})
-    
+        return render(request, 'pages/product.html', {'form' : form})
+
 def handle_uploaded_file(f):
     if not os.path.exists(settings.MEDIA_ROOT):
         os.makedirs(settings.MEDIA_ROOT)
-        
-    file=os.path.join(settings.MEDIA_ROOT, f.name)
+    
+    file = os.path.join(settings.MEDIA_ROOT, f.name)
     with open(file, "wb+") as destination:
         for chunk in f.chunks():
             destination.write(chunk)
-            
+      
+    # encode this image and print(encoded)
+    #encode = encode_image(file);
+    
+    response = process_image(file)
+    print (response.json())
+    return response.json()['choices'][0]['message']['content']
+    
+def life(request):
+    return render(request, "pages/life.html")
